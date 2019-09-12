821a4d6d97d8eed19442068742c0f0e23be14bac
Dawid Kozakiewicz <turgor1996@gmail.com>
2019-09-12 23:18:03 +0200

    add chat window and event handelrs

--- index.js
+++ index.js
@@ -16,4 +16,15 @@ io.on('connection', (socket) => {
 
     console.log('made socket connection', socket.id);
 
+    // Handle chat event
+    socket.on('chat', function(data){
+        // console.log(data);
+        io.sockets.emit('chat', data);
+    });
+
+    // Handle typing event
+    socket.on('typing', function(data){
+        socket.broadcast.emit('typing', data);
+    });
+
 });
